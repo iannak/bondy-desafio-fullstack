653f75446e9c7d18d867c4b49b340c45808a59f6
Anna Franz <franz.karols@gmail.com>
2025-02-14 14:10:21 -0300

    created error handling

--- packages/backend/src/graphql/resolvers/mutations/mutationTest.ts
+++ packages/backend/src/graphql/resolvers/mutations/mutationTest.ts
@@ -1,6 +1,7 @@
 import { connection } from '../../../memoryDB/connection'
 import { User } from '../../../models/User'
 import bcrypt from 'bcrypt'
+import { GraphQLError } from 'graphql'
 
 export const mutationTest = async (_parent, args, _context, _info) => {
   return args.test
@@ -11,20 +12,35 @@ export const loginMutation = {
     try {
       const user = await User.findOne({ email });
       if (!user) {
-        throw new Error('Usuário não encontrado');
+        throw new GraphQLError('Usuário não encontrado', {
+          extensions: {
+            code: 'USER_NOT_FOUND',
+            http: { status: 401 }
+          }
+        });
       }
 
       const isPasswordValid = await bcrypt.compare(password, user.password);
       if (!isPasswordValid) {
-        throw new Error('Senha inválida');
+        throw new GraphQLError('Senha inválida', {
+          extensions: {
+            code: 'INVALID_PASSWORD',
+            http: { status: 401 }
+          }
+        });
       }
 
       return user;
     } catch (error) {
-      if (error instanceof Error) {
-        throw new Error(error.message);
+      if (error instanceof GraphQLError) {
+        throw error;
       }
-      throw new Error('Erro ao realizar login');
+      throw new GraphQLError('Erro ao realizar login', {
+        extensions: {
+          code: 'INTERNAL_SERVER_ERROR',
+          http: { status: 500 }
+        }
+      });
     }
   }
 };
