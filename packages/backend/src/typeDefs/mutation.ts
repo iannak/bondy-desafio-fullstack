5cee3f139fd75ab6a7375606720b32f85334124c
Anna Karolina Franz <anna.franz@e-deploy.com.br>
2025-09-18 19:07:36 -0400

    feat(backend): adicionar mutation login ao schema e resolver

--- packages/backend/src/typeDefs/mutation.ts
+++ packages/backend/src/typeDefs/mutation.ts
@@ -1,7 +1,15 @@
 import gql from 'graphql-tag'
 
 export default gql`
+  type User {
+    _id: ID!
+    name: String!
+    email: String!
+    company: String
+    password: String!
+  }
   type Mutation {
     mutationTest(test: Boolean): Boolean
+    login(email: String!, password: String!): User
   }
 `
